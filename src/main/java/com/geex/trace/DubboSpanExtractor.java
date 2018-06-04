23e947b7742fc4d680adb943f33a27a5b1c1c2a8
zhangyi <zhangyi@geexfinance.com>
2018-06-04 16:45:48 +0800

    Update: dubbo span add parameter types increase the judgment.

--- src/main/java/com/geex/trace/DubboSpanExtractor.java
+++ src/main/java/com/geex/trace/DubboSpanExtractor.java
@@ -65,7 +65,8 @@ public class DubboSpanExtractor implements SpanExtractor<RpcContext> {
         }
 
         // 方法上有参数的情况
-        if (carrier.getParameterTypes() != null) {
+        if (carrier.getParameterTypes() != null &&
+                carrier.getParameterTypes().length > 0) {
             Map<String, String> tags = new HashMap<>(10);
             for (int i = 0; i < carrier.getParameterTypes().length; i++) {
                 String parameterType = carrier.getParameterTypes()[i].getCanonicalName();
